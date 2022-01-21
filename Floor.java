62f2495edc8a0ed79fa446ff6f36e7cbfa87b1ad
System-out-print-Zain <97549504+System-out-print-Zain@users.noreply.github.com>
2022-01-21 13:48:29 -0500

    Update Floor.java

--- Floor.java
+++ Floor.java
@@ -15,5 +15,10 @@ public class Floor extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+    }   
+    public Floor(){
+        GreenfootImage floorImg = getImage();
+        floorImg.scale(200,20);
+        setImage(floorImg);
+    }
 }
