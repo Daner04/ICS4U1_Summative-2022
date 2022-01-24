a0dfa898f002c9b3d4b0a30613045d2348fa1689
Daner04 <97550645+Daner04@users.noreply.github.com>
2022-01-24 13:14:54 -0500

    Update DK.java
    added your change, however I still have to scale the image of do throwing, and make it so he is not floating in the background class

--- DK.java
+++ DK.java
@@ -2,6 +2,7 @@ import greenfoot.*;
 public class DK extends Actor
 {
     long lastTime;
+  
     public void act() 
     {
         if(isTouching(Mario.class))
@@ -12,13 +13,21 @@ public class DK extends Actor
         }
         if(System.currentTimeMillis() - lastTime > 2500)
         {
-           lastTime = System.currentTimeMillis();
-            setImage("Donkey_Kong_Classic_NES_Artwork.png");
+            lastTime = System.currentTimeMillis();
+             setImage("Donkey_Kong_Classic_NES_Artwork.png");    
             getWorld().addObject(new Barrel(), getX(), getY());
+           
+
         }
-                 if (System.currentTimeMillis() - lastTime > 300) {
+           if (System.currentTimeMillis() - lastTime > 300) {
            setImage("donkey-kong_standing.png");
         }
-
+      
     } 
+    public DK(){
+        GreenfootImage dkImg = getImage();
+        dkImg.scale(110,110);
+        setImage(dkImg);
+    }
+   
 }
