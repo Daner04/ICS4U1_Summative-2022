96408829f13cac1cb5899c7a2af12aa16e4e1743
Daner04 <97550645+Daner04@users.noreply.github.com>
2022-01-24 13:12:50 -0500

    Update Finish.java
    I made it a 600 x 400 like it asked made Mario and the princess face each other, fixed the you wins placement and got rid of unnecessary flooring!

--- Finish.java
+++ Finish.java
@@ -8,23 +8,19 @@ import greenfoot.*;
  */
 public class Finish extends World
 {
-
-    /**
-     * Constructor for objects of class Finish.
-     * 
-     */
+    public static int screenX = 600;
+    public static int screenY = 400;
+ 
     public Finish()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1500, 1000, 1); 
-        addObject(new Floor2(), 125, 1000);
-        addObject(new Floor2(), 375, 1000);
-        addObject(new Floor2(), 625, 1000);
-        addObject(new Floor2(), 875, 1000);
-        addObject(new Floor2(), 1125, 1000);
-        addObject(new Floor2(), 1375, 1000);
-        addObject(new Princess(), 650, 940);
-        addObject(new Mario(), 750, 940);
-        showText("YOU WIN",750, 600);
+        super(screenX, screenY, 1); 
+        addObject(new Floor2(),0 , 400);
+        addObject(new Floor2(),200 , 400);
+        addObject(new Floor2(),400 , 400);
+        addObject(new Floor2(),600 , 400);
+        addObject(new Princess(), 200, 350);
+        addObject(new Mario(), 300, 350);
+        showText("YOU WIN",300, 100);
     }
 }
