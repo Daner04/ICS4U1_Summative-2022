2278c4c328fdc7f5359e0daf3cc9bdac360604eb
System-out-print-Zain <97549504+System-out-print-Zain@users.noreply.github.com>
2022-01-26 13:58:47 -0500

    Update Princess.java

--- Princess.java
+++ Princess.java
@@ -14,6 +14,16 @@ public class Princess extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if(isTouching(Mario.class))
+        {
+            Greenfoot.setWorld(new Finish());
+
+            Greenfoot.stop();
+        }
+    }  
+    public Princess(){
+        GreenfootImage princeImg = getImage();
+        princeImg.scale(45,45);
+        setImage(princeImg);
+    }
 }
